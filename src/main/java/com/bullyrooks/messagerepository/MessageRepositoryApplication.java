8a0a48e9b7b2d4df1425d0d87955a8667d832627
brian <brian.rook@gmail.com>
2022-02-21 16:46:39 -0700

    initial commit

---
+++ src/main/java/com/bullyrooks/messagerepository/MessageRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.bullyrooks.messagerepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bullyrooks/messagerepository/MessageRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bullyrooks.messagerepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
